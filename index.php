c1751df86ba5637e531af80a0e53658acd01a298
mahroosh22 <mahroosh222001>
2026-09-01 22:50:27 +0500

    Update index page

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 
 require_once "config/database.php";
 
-echo "Maan Ghafar Garments";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maan Ghafar Garments</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <h1>Maan Ghafar Garments</h1>
+
+</body>
+</html>
